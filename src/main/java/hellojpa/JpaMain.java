e86332c0e62e4dac5b64e0e52d8c04934690e225
bongj9 <lollypark501@naver.com>
2024-03-24 15:18:11 +0900

    Feat:HelloA추가

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -8,9 +8,21 @@ public class JpaMain {
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
-        //code
 
-        em.close();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        try {
+            Member member = new Member();
+            member.setId(1L);
+            member.setName("HelloA");
+            em.persist(member);
+            tx.commit();
+
+        } catch (Exception e) {
+            tx.rollback();
+        }finally {
+            em.close();
+        }
         emf.close();
     }
 }
